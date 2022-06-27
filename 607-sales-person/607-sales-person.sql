34c7c52952da1c9d10cd02dc2b6ccf7488db8284
Anup Dey <anupdey99@gmail.com>
2022-06-27 13:27:51 +0600

    Time: 1749 ms (20.22%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -14,16 +14,23 @@
 
 
 # Join 993 ms
-SELECT name
-FROM SalesPerson
-WHERE name NOT IN
-    ( 
-        SELECT s.name 
-        FROM SalesPerson s
-        JOIN Company c
-        JOIN Orders o
-        ON s.sales_id = o.sales_id AND c.com_id = o.com_id
-        WHERE c.name = 'RED'
-    )
+# SELECT name
+# FROM SalesPerson
+# WHERE name NOT IN
+#     ( 
+#         SELECT s.name 
+#         FROM SalesPerson s
+#         JOIN Company c
+#         JOIN Orders o
+#         ON s.sales_id = o.sales_id AND c.com_id = o.com_id
+#         WHERE c.name = 'RED'
+#     )
 
+select SalesPerson.name
+from SalesPerson
+where SalesPerson.name not in
+( select salesperson.name from
+SalesPerson join Company join Orders
+ON SalesPerson.sales_id = Orders.sales_id and Company.com_id = Orders.com_id
+where Company.name = 'RED')
 
