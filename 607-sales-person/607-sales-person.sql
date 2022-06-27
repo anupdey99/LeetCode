4a6317001667af8b04480d277e9d06ace80cbf62
Anup Dey <anupdey99@gmail.com>
2022-06-27 13:20:49 +0600

    Time: 993 ms (94.46%), Space: 0B (100.00%) - LeetHub

--- 607-sales-person/607-sales-person.sql
+++ 607-sales-person/607-sales-person.sql
@@ -1,16 +1,22 @@
 # Write your MySQL query statement below
-SELECT name 
-FROM SalesPerson
-WHERE sales_id NOT IN (
-    SELECT sales_id 
-    FROM Orders
-    WHERE com_id = (
-        SELECT com_id 
-        FROM Company
-        WHERE name = 'RED'
-    )
-)
+# SELECT name 
+# FROM SalesPerson
+# WHERE sales_id NOT IN (
+#     SELECT sales_id 
+#     FROM Orders
+#     WHERE com_id = (
+#         SELECT com_id 
+#         FROM Company
+#         WHERE name = 'RED'
+#     )
+# )
 
-
+select SalesPerson.name
+from SalesPerson
+where SalesPerson.name not in
+( select salesperson.name from
+SalesPerson join Company join Orders
+ON SalesPerson.sales_id = Orders.sales_id and Company.com_id = Orders.com_id
+where Company.name = 'RED')
 
 
