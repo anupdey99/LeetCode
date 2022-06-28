fa4d48233fb6eeea0dc7a257b980d7c364ceeff3
Anup Dey <anupdey99@gmail.com>
2022-06-29 02:02:39 +0600

    Time: 301 ms (44.71%), Space: 18.6 MB (22.18%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,13 +4,16 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         n = len(s)
-        idx1 = 0
-        idx2 = n - 1
-        while idx1 < idx2:
-            temp = s[idx2]
-            s[idx2] = s[idx1]
-            s[idx1] = temp
-            idx1 += 1
-            idx2 -= 1 
+        # pointer O(n) O(1)
+        # idx1 = 0
+        # idx2 = n - 1
+        # while idx1 < idx2:
+        #     temp = s[idx2]
+        #     s[idx2] = s[idx1]
+        #     s[idx1] = temp
+        #     idx1 += 1
+        #     idx2 -= 1 
         
-        
+        # another approch
+        for i in range(0, (n//2)):
+            s[i], s[n-1-i] = s[n-1-i], s[i]
