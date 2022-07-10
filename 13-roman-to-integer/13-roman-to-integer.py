d9198b4668cd3366dabf81551fdad58bd18b0585
Anup Dey <anupdey99@gmail.com>
2022-07-10 19:20:05 +0600

    Time: 81 ms (40.24%), Space: 13.9 MB (30.63%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        sum = 0
+        prev = 0
+        for i in s[::-1]:
+            curr = map[i]
+            if prev > curr:
+                sum -= curr
+            else:
+                sum += curr
+            prev = curr
+        return sum
+            
+            
+        
